ab307f16d48674193b7fe5bb139c4995a9c7054b
veh0f <ali.vehof1@gmail.com>
2017-08-17 23:00:20 -0400

    added scikit-learn to perceptron

--- perceptron.py
+++ perceptron.py
@@ -4,6 +4,13 @@ import numpy as np
 
 from matplotlib.colors import ListedColormap
 
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
+
+
 
 """Training a perceptron model on the iris dataset"""
 
@@ -87,7 +94,7 @@ plt.show()
 
 
 def plot_decision_regions(X,y, classifier, resolution=0.02):
-    #setup marker generator and color map
+    #marker gen and color map
     markers = ('s','x','o','^','v')
     colors = ('red','blue','lightgreen','gray','cyan')
     cmap = ListedColormap(colors[:len(np.unique(7))])
@@ -99,8 +106,7 @@ def plot_decision_regions(X,y, classifier, resolution=0.02):
     x2_max = X[:, 1].max() + 1
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
     
-    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-    
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)    
     Z = Z.reshape(xx1.shape)
                            
     plt.xlim(xx1.min(), xx1.max())
@@ -117,3 +123,109 @@ plt.xlabel('sepal length [cm]')
 plt.ylabel('petal length [cm]')
 plt.legend(loc='upper right')
 plt.show()
+
+
+'''Training perceptron via scikit-learn'''
+
+iris = datasets.load_iris()
+X= iris.data[:,[2,3]]
+y= iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=0)
+
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+ppn = Perceptron(n_iter=40, eta0=0.1, random_state=0)
+ppn.fit(X_train_std, y_train)
+y_pred = ppn.predict(X_test_std)
+print('Misclassified samples: %d' %(y_test != y_pred).sum())
+print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
+
+
+'''highlight the samples from the data set with marker generator and color map'''
+
+#plot_decision regions as prev function with modification at the bottom
+#ploting all the samples. Specifying the indecies of 
+#the samples we want to mark on the resulting plot 
+
+
+def plot_decision_regions0(X, y, classifier, test_idx=None, resolution=0.05):
+    #marker gen and color map
+    markers = ('s','x','o','^','v')
+    colors = ('red','blue', 'lightgreen','gray','cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    #plot the decision surface
+    x1_min = X[:, 0].min() - 1
+    x1_max = X[:, 0].max() + 1
+    x2_min = X[:, 1].min() - 1
+    x2_max = X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
+    
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+
+    #plot all samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha = 0.8, c=cmap(idx), marker=markers[idx], label=cl)
+
+
+    #highlight test samples
+    if test_idx:
+        X_test = X[test_idx, :]
+        y_test = y[test_idx]
+        plt.scatter(X_test[:,0],X_test[:,1], c='',alpha=1.0,linewidths=1,marker='o',s=55,label='test set')
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions0(X=X_combined_std, y=y_combined, classifier=ppn, test_idx=range(105,150))
+
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='lower right')
+plt.show()
+
+
+
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
